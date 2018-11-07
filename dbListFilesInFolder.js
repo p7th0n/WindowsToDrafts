dbaabf6642905aa31689511af8549af772c463b4
Dave Kurman <dkurman@netsnbytes.com>
2018-11-07 15:47:30 -0500

    Get file list

--- dbListFilesInFolder.js
+++ dbListFilesInFolder.js
@@ -1,13 +1,14 @@
 ﻿// List Dropbox Files in Folder
 // https://forums.getdrafts.com/t/examples-of-use-of-new-dropbox-integration/2572
 
-var folder = draft.processTemplate("[[safe_title]]");
+// var folder = draft.processTemplate("[[safe_title]]");
+var folder = '/drafts/';
 
 // create Dropbox object and vars
 let db = Dropbox.create();
 let endpoint = "https://api.dropboxapi.com/2/files/list_folder";
 let args = {
-    "path": "/Shared Folders/Drafts/" + folder + "/",
+    "path": folder,
     "recursive": false,
     "include_media_info": false,
     "include_deleted": false,
@@ -16,27 +17,25 @@ let args = {
 };
 
 // make API request
-let response = db.rpcRequest({
-	"url": endpoint,
-	"method": "POST",
-	"data": args
-});
-if (response.statusCode != 200) {
-	console.log("Dropbox Error: " + response.statusCode + ", " + response.error);
-	context.fail();
+let response = db.rpcRequest( {
+    "url": endpoint,
+    "method": "POST",
+    "data": args
+} );
+if ( response.statusCode != 200 ) {
+    console.log( "Dropbox Error: " + response.statusCode + ", " + response.error );
+    context.fail();
 } else {
-	let p = Prompt.create();
-	p.title = "Pick a file";
-	let fileList = response.responseData.entries;
-	Object.keys(fileList).forEach(function(key) {
-		if (!fileList[key].name.endsWith('.md') && !fileList[key].name.endsWith('.html')) {
-			p.addButton(fileList[key].name);
-		}
-	});
-	let didSelect = p.show();
-	if (didSelect) {
-		editor.setSelectedText("![](" + p.buttonPressed + ")");
-		let selectionRange = editor.getSelectedRange();
-		editor.setSelectedRange(editor.getSelectedRange()[0]+2, 0);
-	}
+    let p = Prompt.create();
+    p.title = "Pick a file";
+    let fileList = response.responseData.entries;
+    Object.keys( fileList ).forEach( function( key ) {
+        p.addButton( fileList[ key ].name );
+    } );
+    let didSelect = p.show();
+    if ( didSelect ) {
+        editor.setSelectedText( "![](" + p.buttonPressed + ")" );
+        let selectionRange = editor.getSelectedRange();
+        editor.setSelectedRange( editor.getSelectedRange()[ 0 ] + 2, 0 );
+    }
 }
