8c457b8f095569aa56a606b0678c76cfd806e66e
Dave Kurman <dkurman@netsnbytes.com>
2018-11-20 10:39:42 -0500

    Update readme; add comments to js script

---
+++ DropboxToDrafts.js
@@ -0,0 +1,70 @@
+﻿// DropboxToDrafts.js
+//
+// Import file content from files in a 'Drafts' folder on Dropbox
+// as individual Drafts.
+//
+// Dropbox is used to pipe text _Drafts_ from Windows or Linux
+// to the Drafts app on iOS.
+
+let msg = '';
+let fileCount = 0;
+var folder = '/drafts/';
+
+// create Dropbox object and vars
+let db = Dropbox.create();
+let apiListFolder = "https://api.dropboxapi.com/2/files/list_folder";
+var apiDeleteV2 = 'https://api.dropboxapi.com/2/files/delete_v2';
+
+// get Dropbox API file list from 'Drafts' folder 
+let listFolderArgs = {
+    "path": folder,
+    "recursive": false,
+    "include_media_info": false,
+    "include_deleted": false,
+    "include_has_explicit_shared_members": false,
+    "include_mounted_folders": true
+};
+let response = db.rpcRequest( {
+    "url": apiListFolder,
+    "method": "POST",
+    "data": listFolderArgs
+} );
+if ( response.statusCode != 200 ) {
+    alert( "Dropbox Error: " + response.statusCode + ": " + response.error );
+} else {
+    msg = "Import Dropbox files to Drafts...\n";
+    let dbDraftsList = response.responseData.entries;
+    Object.keys( dbDraftsList ).forEach( function( key ) {
+
+        // Get Dropbox file content
+        let fileName = dbDraftsList[ key ].name
+        let filePath = folder + fileName
+        msg += '* ' + fileName + '\n';
+        var fileContents = db.read(filePath);
+        // TODO: check for errors reading content like Windows/DOS line endings [CR-LF]
+
+        // Create a new Draft for each Dropbox file
+        var d = Draft.create();
+        d.content = fileContents;
+        d.update();                     // save Draft
+        fileCount += 1;                 // count how many Drafts imported
+
+        // delete Dropbox file once a Draft is created
+        // to avoid importing the same content again.
+        let deleteArgs = {
+            'path': folder + fileName
+        }
+        let deleteResponse = db.rpcRequest( {
+            'url': apiDeleteV2,
+            'method': 'POST',
+            'data': deleteArgs
+        })
+    });
+    // show a message when import completes
+    if (fileCount > 0) {
+        msg += '\n' + fileCount.toString() + ' files imported.'
+        alert(msg);
+    } else {
+        alert(msg + '\nNo files found!');
+    }
+}
--- README.md
+++ README.md
@@ -5,37 +5,21 @@
 * Like the tagline says, where text starts. It is such a handy app and concept. But I spend a lot of time in Windows.
 * Get content from Windows into Drafts App
 * Get Windows command line output to Drafts. Wrapper scripts take commands and redirect output to a Dropbox file, overwriting previous content.
+* Copy the content from _DropboxToDrafts.js_ to a Drafts action.
+* Or import from the Drafts Actions directory.
 
 ## Ideas
 
 * Develop a process to create Drafts with content from existing files.  This is useful with the iOS Workflow/Shortcuts app too.  And iOS Shortcuts are easy to automate with Siri voice commands.
-* Instead of using one file, use a single purpose folder. Drafts checks all files. When the Drafts action imports the draft it will erase the content. The Windows wrapper will delete files with no content.
+* Instead of using one file, use a single purpose folder. Drafts checks all files. When the Drafts action imports the draft it will erase the content.
 
 ## Problems
 
 * DOS file formats (CR/LF). I use a dos2unix function in the PowerShell profile to fix file format issues.
 * Drafts App is iOS and macOS beta only.
 
-```javascript
-/*  
-*   Drafts App Action 'FromWindows'
-*
-*       Creates a new Draft with the contents of a Dropbox file
-*
-*/
+## References
 
-var db = Dropbox.create();
-var filePath = '/Notes/fromWindows.txt';
-var d = Draft.create();
-
-var fileContents = db.read(filePath);
-
-d.content = fileContents;
-d.update();
-
-```
-
-## Version 2
-
+* [Drafts Community forum](https://forums.getdrafts.com/t/examples-of-use-of-new-dropbox-integration/2572)
 * [Dropbox v2 /list_folder](https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder)
 * [Dropbox v2 /delete](https://www.dropbox.com/developers/documentation/http/documentation#files-delete)
